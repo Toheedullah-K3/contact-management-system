6ee21ac0c12a8e87934c819bdbc8b44a7df29ea0
Toheedullah-K3 <108226737+Toheedullah-K3@users.noreply.github.com>
2024-08-11 02:44:15 +0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'https://dev-contact-management-system-ermtq48hx.vercel.app/',
+    origin: 'https://contact-management-system-nine.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
