2c99aa508d2734d8e885a8aa813bea8b0e0709e1
Toheedullah-K3 <108226737+Toheedullah-K3@users.noreply.github.com>
2024-08-11 13:00:30 +0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,9 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
