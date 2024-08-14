03ec0df7d4ef03c46f1ea586cd4875f76e707657
Toheedullah-K3 <toheedullah002@gmail.com>
2024-08-14 21:49:16 +0500

    url endpoint set

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: 'https://contact-management-system-client.vercel.app/',
+    origin: 'https://contact-management-system-client.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
